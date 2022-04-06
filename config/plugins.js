546f5729c860f406c8b0ebc8b9465cecd3a5b3ad
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-04-06 14:26:24 +0200

    6

--- config/plugins.js
+++ config/plugins.js
@@ -9,5 +9,15 @@ module.exports = ({ env }) => ({
         xsmall: 64,
       },
     },
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
   },
 });
